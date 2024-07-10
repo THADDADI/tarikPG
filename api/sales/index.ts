862efd2767b9c6ca63f1121b5480a31f9d8b7f61
HADDADI Tarik <thaddadi@tls-lux.tgl>
2024-07-10 14:40:45 +0200

    fix(ci/cd): fixing backend configs

--- api/sales/index.ts
+++ api/sales/index.ts
@@ -11,7 +11,7 @@ export const salesFunctions = async (
   context: InvocationContext
 ): Promise<HttpResponseInit | HttpResponse> => {
   context.log("HTTP trigger function processed a request.");
-  const name = req.params.name || (req.body && req.params.name);
+  const name = req.query || (req.body && req.params.name);
   const responseMessage = name
     ? "Hello, " + name + ". This HTTP triggered function executed successfully."
     : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
